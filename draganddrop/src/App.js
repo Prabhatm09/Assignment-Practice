2e935765178955f7a4dc600d0a02b2a53ef5678b
Prabhat mishra <105411899+Prabhatm09@users.noreply.github.com>
2022-12-16 22:38:15 +0530

    drag & drop

---
+++ draganddrop/src/App.js
@@ -0,0 +1,12 @@
+import DragDrop from "./DragDrop/index"
+
+
+function App() {
+  return (
+    <div>
+      <DragDrop />
+    </div>
+  );
+}
+
+export default App;
---
+++ draganddrop/src/index.js
@@ -0,0 +1,32 @@
+
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  rootElement
+);
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from "./reportWebVitals"
+// import "antd/dist/antd.css"
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// reportWebVitals();
+
