53297c0583d82b8a3e9bed47679e3dd7bc8e5b91
Prabhat <prabhatmishra1409@gmail.com>
2023-05-30 12:40:25 +0530

    check

--- long/src/components/table/ChartMap.jsx
+++ long/src/components/table/ChartMap.jsx
@@ -1,26 +1,24 @@
-import React, { useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Chart } from "primereact/chart";
 
 function ChartMap({ bg, value }) {
-  const [chartData,setChartData] = useState();
+  const [chartData, setChartData] = useState();
 
+  console.log(chartData);
   //To Set the onchange data
-  useEffect(()=>{
-   setChartData({
-    datasets: [
-      {
-        data: [value, 100 - value],
-        backgroundColor: [bg, "#C4C7CF"],
-      },
-    ],
-  })
-  },[bg, value])
+  useEffect(() => {
+    setChartData({
+      datasets: [
+        {
+          data: [value, 100 - value],
+          backgroundColor: [bg, "#C4C7CF"],
+        },
+      ],
+    });
+  }, [bg, value]);
   return (
     <>
-      <Chart
-        type="doughnut"
-        data={chartData}
-      />
+      <Chart type="doughnut" data={chartData} />
     </>
   );
 }
